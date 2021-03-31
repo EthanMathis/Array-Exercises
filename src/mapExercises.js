427cbf5e0f229d9a73a79137ab5f8da0bea63cf6
EthanMathis <ethan.mathis@outlook.com>
2021-03-31 11:48:20 -0500

    finished map exercises and all but 1 of the find exercises

--- src/mapExercises.js
+++ src/mapExercises.js
@@ -7,6 +7,10 @@ const instructors = useInstructors();
 // It should not accept any parameters`
 // It should return an array of strings. Each string should be the full name of the student
 
+export const getStudentNames = () => {
+  return students.map(kid => `${kid.firstName} ${kid.lastName}`)
+}
+
 // Export a function named StudentList
 // It should not accept any parameters
 // It should return a single HTMLString that contains a <div> element for every student that looks like the following:
@@ -17,3 +21,13 @@ const instructors = useInstructors();
     <h2>Cohort 43</h2>
   </div>
 */
+
+export const StudentList = () => {
+  return students.map(kid => {
+    return `
+    <div>
+    <h1>${kid.firstName} ${kid.lastName}</h1>
+    <h2>Cohort ${kid.cohort}</h2>
+    </div>`
+  })
+}
